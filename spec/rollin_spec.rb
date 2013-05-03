d3bb3e18a3b172e930c73d3ce9028d23d76d37e2
marano <thiagomarano@gmail.com>
2013-05-04 00:01:49 +0800

    Deletes those crapy tests and creates one big test file to be used as documentation.

---
+++ spec/rollin_spec.rb
@@ -0,0 +1,76 @@
+require 'spec_helper'
+
+# Fixtures
+#
+# 2013_05_01_My_first_post.mk
+# 2013_05_02_My_second_post.mk
+# 2013_06_01_My_third_post.mk
+# 2014_01_01_My_fourth_post.mk
+
+describe 'how rollin works' do
+
+  subject (:blog) { Rollin::Blog.new(articles_folder: 'spec/fixtures') }
+
+  context 'retrieving posts' do
+    let (:first_article) { TestArticle.new(id: '2013_05_01_My_first_post', title: 'My first post', date: Date.new(2013, 5, 1)) }
+    let (:second_article) { TestArticle.new(id: '2013_05_02_My_second_post', title: 'My second post', date: Date.new(2013, 5, 2)) }
+    let (:third_article) { TestArticle.new(id: '2013_06_01_My_third_post', title: 'My third post', date: Date.new(2013, 6, 1)) }
+    let (:fourth_article) { TestArticle.new(id: '2014_01_01_My_fourth_post', title: 'My fourth post', date: Date.new(2014, 1, 1)) }
+
+    it 'gets articles' do
+      # articles
+
+      blog.should have(4).articles
+
+      blog.articles[0].should == first_article
+      blog.articles[1].should == second_article
+      blog.articles[2].should == third_article
+      blog.articles[3].should == fourth_article
+    end
+
+    it 'provides monthly archive' do
+      blog.monthly_archive.should have(3).articles
+
+      blog.monthly_archive[0].year.should == 2013
+      blog.monthly_archive[0].month.should == 5
+      blog.monthly_archive[0].should have(2).articles
+      blog.monthly_archive[0].articles.should include(first_article, second_article)
+
+      blog.monthly_archive[1].year.should == 2013
+      blog.monthly_archive[1].month.should == 6
+      blog.monthly_archive[1].should have(1).articles
+      blog.monthly_archive[1].articles.should include(third_article)
+
+      blog.monthly_archive[2].year.should == 2014
+      blog.monthly_archive[2].month.should == 1
+      blog.monthly_archive[2].should have(1).articles
+      blog.monthly_archive[2].articles.should include(fourth_article)
+    end
+
+    it 'provides annual archive' do
+      blog.should have(2).annual_archive
+
+      blog.annual_archive[0].year.should == 2013
+      blog.annual_archive[0].should have(2).monthly_archive
+      blog.annual_archive[0].monthly_archive[0].year.should == 2013
+      blog.annual_archive[0].monthly_archive[0].month.should == 5
+      blog.annual_archive[0].monthly_archive[0].should have(2).articles
+      blog.annual_archive[0].monthly_archive[0].articles.should include(first_article, second_article)
+      blog.annual_archive[0].monthly_archive[1].year.should == 2013
+      blog.annual_archive[0].monthly_archive[1].month.should == 6
+      blog.annual_archive[0].monthly_archive[1].should have(1).articles
+      blog.annual_archive[0].monthly_archive[1].articles.should include(third_article)
+      blog.annual_archive[0].should have(3).articles
+      blog.annual_archive[0].articles.should include(first_article, second_article, third_article)
+
+      blog.annual_archive[1].year.should == 2014
+      blog.annual_archive[1].should have(1).monthly_archive
+      blog.annual_archive[1].monthly_archive[0].year.should == 2014
+      blog.annual_archive[1].monthly_archive[0].month.should == 1
+      blog.annual_archive[1].monthly_archive[0].should have(1).articles
+      blog.annual_archive[1].monthly_archive[0].articles.should include(fourth_article)
+      blog.annual_archive[1].should have(1).articles
+      blog.annual_archive[1].articles.should include(fourth_article)
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,19 @@ require 'bundler/setup'
 Bundler.require :development
 
 require './lib/rollin'
+
+class Rollin::Article
+  def ==(other)
+    id == other.id && title == other.title && date == other.date
+  end
+end
+
+class TestArticle
+  attr_reader :id, :title, :date
+
+  def initialize(properties)
+    @id = properties[:id]
+    @title = properties[:title]
+    @date = properties[:date]
+  end
+end
